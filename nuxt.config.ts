e44f13b4a2ed03154b1574472219ac9af9de829c
Vince489 <vincentcornelius489@gmail.com>
2024-02-18 08:03:30 -0500

    yup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'theme-color', content: '#374151' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        { name: 'description', content: '' },
+        { name: 'description', content: 'Main hub of the VBC platform.' },
       ],
     },
   },
